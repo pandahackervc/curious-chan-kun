fe2ea038ea69b6a2b8a751c76c8e394ec7f8bc3c
vigneshchandar <vignesh.balaguruswamy@zohocorp.com>
2026-02-09 12:00:34 +0530

    Fix Netlify build: remove astro-iconify and use pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
 
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind()],
+    integrations: [mdx(), tailwind()],
     markdown: {
         shikiConfig: {
           // Choose from Shiki's built-in themes (or add your own)
